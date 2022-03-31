7a27bc052d7f15d70c60c1ec5c685d0fe7e99755
JancoBH <jancoboscan@gmail.com>
2022-03-31 03:15:44 -0300

    vitest implementation + init home page + navbar + React 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { AppRouter } from './AppRouter';
 
 function App() {
   return (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <React.StrictMode>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </React.StrictMode>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,28 @@
 export const Home = () => {
+  const sections = [
+    { title: 'Section 1', background: 'bg-gray-900' },
+    { title: 'Section 2', background: 'bg-blue-500' },
+    { title: 'Section 3', background: 'bg-green-500' },
+  ];
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section
+        className="bg-no-repeat bg-cover bg-center text-white flex justify-center items-center h-screen"
+        style={{
+          backgroundImage:
+            'linear-gradient(rgba(52,41,49,.8),rgba(26,33,74,.8)),url("https://image.tmdb.org/t/p/original//56v2KjBlU4XaOv9rVYEQypROD7P.jpg")',
+        }}
+      >
+        <h1 className="text-5xl">Stranger Things</h1>
+      </section>
+      {sections.map((sec) => (
+        <div key={sec.title}>
+          <section className={sec.background + ` text-white flex justify-center items-center h-96`}>
+            <h1>{sec.title}</h1>
+          </section>
+        </div>
+      ))}
+    </>
   );
 };
